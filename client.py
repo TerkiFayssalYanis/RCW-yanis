c3dd87873f417582f6dc175127292033839abe91
TerkiFayssalYanis <faycal.terki@gmail.com>
2025-06-03 09:28:33 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
  
 #url = "http://localhost:8000/"
-url="https://appapiaziz-g9cwbtg8gtfthqcu.canadaeast-01.azurewebsites.net/test"
+url="https://rcw-yanis-dpbyahgrf9bvafdk.canadaeast-01.azurewebsites.net/test"
 response = requests.get(url)
 response = response.json()
 print(response['message'])
